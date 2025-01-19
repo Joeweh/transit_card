45ec89235cf14db29b5d87750706dc7655a89449
Chris Yeo <chrisyeo@Chriss-MacBook-Air.local>
2025-01-18 19:45:55 -0500

    updated home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -10,26 +10,400 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      children: [
+        Container(
+          width: 37,
+          height: 88,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 939,
+          height: 196,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(29),
+            color: Color(0xff5885ec),
+          ),
+        ),
+        Container(
+          width: 938,
+          height: 142,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(29),
+            color: Colors.white,
+          ),
+        ),
+        Image.asset(
+          "assets/fixedmayb 1.png",
+          width: 1347,
+          height: 898,
+        ),
+        Container(
+          width: 314,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Color(0xff5986ed),
+          ),
+        ),
+        Column(
           children: [
-            const Text(
-              'ඞTestඞ',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle button press
-              },
-              child: const Text('Button'),
+            Container(
+              width: 39,
+              height: 28,
             ),
           ],
         ),
-      ),
+        Container(
+          width: 1285,
+          height: 225,
+        ),
+        Stack(
+          children: [
+            Container(
+              width: 118.7149658203125,
+              height: 118.7149658203125,
+              decoration: BoxDecoration(
+                color: Color(0x00ffffff),
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: 98.90618896484375,
+                  height: 98.90679168701172,
+                ),
+                Container(
+                  width: 39.5716552734375,
+                  height: 24.732284545898438,
+                ),
+              ],
+            ),
+          ],
+        ),
+        Stack(
+          children: [
+            Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                color: Color(0x00ffffff),
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: 19.995361328125,
+                  height: 19.9954833984375,
+                ),
+                Container(
+                  width: 8,
+                  height: 5,
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Text(
+          " Balance",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "Portal Card",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        const Text(
+          "Portal Card",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        const Text(
+          "Portal Card",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        const Text(
+          "REWARDS",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        const Text(
+          "GET PAID TO GO PUBLIC",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        Image.asset(
+          "assets/Ellipse 2.png",
+          width: 225,
+          height: 225,
+        ),
+        const Text(
+          "New tokens variables!",
+          style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "New tokens variables!",
+          style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Container(
+          width: 364,
+          height: 239,
+        ),
+        const Text(
+          " Balance",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          " Balance",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "22.75",
+          style: TextStyle(
+            fontSize: 64,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Container(
+          width: 939,
+          height: 37,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Column(
+          children: [
+            Container(
+              width: 98.75,
+              height: 0,
+            ),
+            Container(
+              width: 98.75,
+              height: 0,
+            ),
+            Container(
+              width: 98.75,
+              height: 0,
+            ),
+          ],
+        ),
+        const Text(
+          "Explore",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        Text(
+          "Points",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        Text(
+          "Add Balance",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        Container(
+          width: 939,
+          height: 83,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(35),
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 939,
+          height: 83,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(35),
+            color: Colors.white,
+          ),
+        ),
+        const Text(
+          "2.40",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "13.38",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "MBTA 550003106212",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          "SQ*MAMACITA AUT...",
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Container(
+          width: 34,
+          height: 34,
+          decoration: BoxDecoration(
+            color: Color(0xff23c212),
+          ),
+        ),
+        Container(
+          width: 34,
+          height: 34,
+          decoration: BoxDecoration(
+            color: Color(0xffff0e12),
+          ),
+        ),
+        Container(
+          width: 938,
+          height: 35,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 315,
+          height: 38,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(35),
+            color: Colors.white,
+          ),
+        ),
+        const Text(
+          "See All Transactions",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Container(
+          width: 20,
+          height: 53,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 20,
+          height: 53,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 19,
+          height: 30,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Container(
+          width: 30,
+          height: 72,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        Column(
+          children: [
+            Container(
+              width: 18.8799991607666,
+              height: 16.456270217895508,
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              width: 103,
+              height: 103,
+            ),
+          ],
+        ),
+        Image.asset(
+          "assets/Ellipse 1.png",
+          width: 178,
+          height: 162,
+        ),
+        Column(
+          children: [
+            Container(
+              width: 24.159040451049805,
+              height: 24.77291488647461,
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              width: 102,
+              height: 104,
+            ),
+          ],
+        ),
+      ],
     );
+
+    // return Scaffold(
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         const Text(
+    //           'ඞTestඞ',
+    //           style: TextStyle(fontSize: 24),
+    //         ),
+    //         const SizedBox(height: 20),
+    //         ElevatedButton(
+    //           onPressed: () {
+    //             // Handle button press
+    //           },
+    //           child: const Text('Button'),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
 
     //return const Placeholder();
   }
