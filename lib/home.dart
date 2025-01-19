0d7d8f6e02d5dfd1a1da415d5933e95349c19574
Joeweh <joeyqsa@outlook.com>
2025-01-19 03:39:25 -0500

    Changelog:
    - Cleanup files
    - Add "Add to Google Wallet" To HomePage
    - Adjust values

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_wallet/google_wallet.dart';
+import 'package:transit_card/api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +11,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Color> _colors = [ Colors.green, Colors.red, Colors.blue ];
-  final List<String> _titles = [ "MBTA 505236", "SQ*MAMACITAS AUT...", "CalTrain 8694035" ];
-  final List<String> _costs = [ "\$2.40", "\$13.38", "\$4.80" ];
+  final List<Color> _colors = [ Colors.green, Colors.red ];
+  final List<String> _titles = [ "MBTA 505236", "SQ*MAMACITAS AUT..." ];
+  final List<String> _costs = [ "\$2.40", "\$13.38" ];
+
+  final wallet = GoogleWallet();
 
   @override
   void initState() {
@@ -48,7 +52,7 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        height: MediaQuery.of(context).size.height / 7,
+        height: MediaQuery.of(context).size.height / 8.65,
         shape: const CircularNotchedRectangle(),
         child: Container(
           margin: const EdgeInsets.symmetric(horizontal: 10.0),
@@ -72,11 +76,11 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               const Column(
-                spacing: 2.0,
+                spacing: 1.75,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
-                    height: 15,
+                    height: 36.75,
                   ),
                   Text(
                     'Pay',
@@ -123,14 +127,14 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               const SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
               Image.asset(
                 "assets/images/cards/PortalCardBackShadow.png",
                 scale: 2.0,
               ),
               const SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
               Card(
                 margin: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -165,12 +169,23 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Container(
                       margin: const EdgeInsets.fromLTRB(0.0, 20.0, 22.0, 0.0),
-                      child: const Text(
-                        '\$29.34',
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                        textAlign: TextAlign.right,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GoogleWalletButton(
+                            style: GoogleWalletButtonStyle.condensed,
+                            onPressed: () async {
+                              await wallet.savePasses(GWalletAPI.formatRequest());
+                            },
+                          ),
+                          const Text(
+                            '\$29.34',
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ],
                       ),
                     ),
 
@@ -178,7 +193,7 @@ class _HomePageState extends State<HomePage> {
                       margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
                       child: ListView.builder(
                         shrinkWrap: true,
-                        itemCount: 3,
+                        itemCount: 2,
                         itemBuilder: (BuildContext context, int index) {
                           return Container(
                             margin: const EdgeInsets.symmetric(horizontal: 15.0),
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:transit_card/home.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_wallet/google_wallet.dart';
-import 'package:transit_card/api.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -16,14 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wallet = GoogleWallet();
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Portal',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromRGBO(218, 218, 218, 1.0),
         useMaterial3: true,
       ),
       home: const HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
